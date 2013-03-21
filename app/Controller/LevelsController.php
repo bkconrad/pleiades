50e93e5094f9822a0980001debc674129a2a104f
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-03-21 16:53:56 -0700

    add level editing

--- app/Controller/LevelsController.php
+++ app/Controller/LevelsController.php
@@ -15,6 +15,35 @@ class LevelsController extends AppController {
     $this->set('levels', $this->Level->find('all'));
   }
 
+  public function edit($id) {
+    $level = $this->Level->findById($id);
+    if(empty($level)) {
+      $this->flash("Level not found", $this->referer());
+    }
+
+    if(!$this->Auth->loggedIn()) {
+      $this->flash("You must be logged in to edit a level", $this->referer());
+    }
+
+    if($level["User"]["user_id"] != $this->Auth->user('user_id')) {
+      $this->flash("You can only edit a level you uploaded", $this->referer());
+    }
+
+    if($this->request->is('post') || $this->request->is('put')) {
+      $this->Level->id = $id;
+      if($this->Level->save($this->request->data)) {
+        $this->Session->setFlash('Level updated');
+        $this->redirect(array('action' => 'edit', $id));
+      } else {
+        $this->Session->setFlash('Unable to update level');
+      }
+    }
+
+    if(empty($this->request->data)) {
+      $this->request->data = $level;
+    }
+  }
+
   public function view($id) {
     $this->set('level', $this->Level->findById($id));
   }
