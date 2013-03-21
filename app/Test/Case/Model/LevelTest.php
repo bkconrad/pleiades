2b97e5362873071baaea7e18203efe771a56df46
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-03-21 15:26:38 -0700

    comments for level rating test

--- app/Test/Case/Model/LevelTest.php
+++ app/Test/Case/Model/LevelTest.php
@@ -11,15 +11,19 @@ class LevelTest extends CakeTestCase {
   }
 
   public function testRating() {
+    // starts at zero
     $this->Level->create();
     $this->assertEquals(0, $this->Level->field('rating'));
 
+    // can be negative
     $this->Level->rate(1, -1);
     $this->assertEquals(-1, $this->Level->field('rating'));
 
+    // replaces old ratings
     $this->Level->rate(1, 1);
     $this->assertEquals(1, $this->Level->field('rating'));
 
+    // accumlates for each level
     $this->Level->rate(2, 1);
     $this->assertEquals(2, $this->Level->field('rating'));
   }
