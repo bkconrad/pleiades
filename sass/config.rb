91fe6c855f05a45a4a669bd87274b5efcae055bf
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-04-28 12:42:36 -0400

    Add sass framework

---
+++ sass/config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+sass_dir = 'src'
+css_dir = '../app/webroot/css'
+images_dir = '../app/webroot/img'
+javascripts_dir = '../app/webroot/js'
+http_stylesheets_path = 'css'
+http_javascripts_path = 'js'
+http_images_path = 'img'
+environment = :development
+output_style = :compressed
