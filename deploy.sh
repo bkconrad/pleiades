aa880d02024dc194d31316daf960d194618a029f
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-04-28 10:00:57 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,15 +15,25 @@ pushd `dirname $0` > /dev/null
 project_root=`pwd`
 popd > /dev/null
 
-# make schema, add a commit
-echo 'Creating schema dump, please enter local MySQL password'
-mysqldump -u $MYSQL_USER -p $MYSQL_DB --add-drop-database --no-data > $project_root/schema.sql
-git add  $project_root/schema.sql
-git commit
+if [ "$1" == "-s" ]
+then
+	echo 'updating schema'
+	UPDATE_SCHEMA=yes
+fi
+
+if [ $UPDATE_SCHEMA ]
+then
+	# make schema, add a commit
+	echo 'Creating schema dump, please enter local MySQL password'
+	mysqldump -u $MYSQL_USER -p $MYSQL_DB --add-drop-database --no-data > $project_root/schema.sql
+	git add  $project_root/schema.sql
+	git commit
+
+	# get MySQL pass
+	read -s -p "Remote MySQL Password: " REMOTE_PASSWORD
+fi
 
 git push $PUSH_REPOSITORY
-
-# read -s -p "Remote MySQL Password: " REMOTE_PASSWORD
 
 ssh_commands=`cat <<EOF
 cd $REMOTE_DIRECTORY
@@ -35,7 +45,11 @@ do
 done
 EOF`
 
+if [ $UPDATE_SCHEMA ]
+then
+	ssh_commands="$ssh_commands ; mysql -u $REMOTE_MYSQL_USER -p$REMOTE_PASSWORD $REMOTE_MYSQL_DB < $REMOTE_DIRECTORY/schema.sql"
+fi
+
 echo "$ssh_commands"
 
 ssh $REMOTE_HOST "$ssh_commands"
-# ssh $REMOTE_HOST "mysql -u $REMOTE_MYSQL_USER -p $REMOTE_MYSQL_DB < $REMOTE_DIRECTORY/schema.sql"
