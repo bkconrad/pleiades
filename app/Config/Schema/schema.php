934c80102f87d49793427b2dd67e937690aae11a
Bryan Conrad <bkconrad@gmail.com>
2014-07-08 21:27:15 -0700

    updated schema

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -28,12 +28,12 @@ class AppSchema extends CakeSchema {
 		'description' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'content' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'levelgen' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'levelgen_filename' => array('type' => 'string', 'null' => true, 'length' => 128, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'levelgen_filename' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 128, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'downloads' => array('type' => 'integer', 'null' => false, 'default' => '0'),
 		'rating' => array('type' => 'integer', 'null' => false, 'default' => null),
 		'tags' => array('type' => 'integer', 'null' => false, 'default' => null),
 		'screenshot_filename' => array('type' => 'string', 'null' => false, 'default' => 'no-screenshot.png', 'length' => 256, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'level_filename' => array('type' => 'string', 'null' => false, 'length' => 256, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'level_filename' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 256, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'created' => array('type' => 'timestamp', 'null' => false, 'default' => 'CURRENT_TIMESTAMP'),
 		'last_updated' => array('type' => 'timestamp', 'null' => false, 'default' => '0000-00-00 00:00:00'),
 		'game_type' => array('type' => 'string', 'null' => false, 'default' => 'Bitmatch', 'length' => 64, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
@@ -53,6 +53,19 @@ class AppSchema extends CakeSchema {
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+
+	public $notifications = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'message' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'url' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'user_id' => array('column' => 'user_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
 	);
 
 	public $ratings = array(
